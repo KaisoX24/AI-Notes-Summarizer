04eb81d45efed7b63d6c02273383c2edb1c949bf
Pramit Acharjya <acharjyapramit@gmail.com>
2026-03-13 12:10:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,14 @@ import streamlit as st
 import os
 import PyPDF2
 from dotenv import load_dotenv
-from supabase import create_client,Client
 
 # Loading environment variables
 load_dotenv()
-supabase_url=os.getenv('Supa_base_url')
-supabase_key=os.getenv('Supa_base_API_KEY')
-supabase: Client=create_client(supabase_url,supabase_key)
 
 client=Groq(
     api_key=os.getenv('GROQ_API_KEY')
 )
 
-
-def signup(email,password):
-    try:
-        user=supabase.auth.sign_up({'email':email,'password':password})
-        return user
-    except Exception as e:
-        st.error(f'Registration Failed: {e}')
-
-def signin(email,password):
-    try:
-        user=supabase.auth.sign_in_with_password({'email':email,'password':password})
-        return user
-    except Exception as e:
-        st.error(f'Sign in Failed: {e}')
-
-def signout():
-    try:
-        supabase.auth.sign_out()
-        st.session_state.user_email=None
-        st.rerun()
-    except Exception as e:
-        st.error(f'Sign out Failed: {e}')
 
 # Function to summarize text and generate quiz questions
 def summarize_text(prompt):
@@ -72,7 +46,7 @@ def summarize_text(prompt):
         return f"Cant Summarize at the Moment"
     
 # Streamlit UI
-def main_app(user):
+def main_app():
     st.set_page_config(page_title='Notes Summarizer and Quizer', page_icon=':books:',layout='wide')
     st.title('Notes Summarizer and Quizer :books:')
     st.write('---')
@@ -101,32 +75,5 @@ def main_app(user):
             file_name='summary_and_quiz.txt',
             mime='application/plain'
         )
-        if st.sidebar.button('Logout'):
-            signout()
+main_app()
 
-def auth_screen():
-    st.set_page_config(layout='centered')
-    st.title('WELCOME TO USER AUTHETICATION')
-    option=st.selectbox('Choose action:',['Login','Sign Up'])
-    email=st.text_input('Enter your email')
-    passw=st.text_input('Password',type='password')
-    if option=='Sign Up' and st.button('Register'):
-        user=signup(email,passw)
-        if user and user.user:
-            st.success('Registration Successful. Please confrim the email')
-            st.markdown('Confirmation Email has been sent')
-
-    elif option=='Login' and st.button('Login'):
-        user=signin(email,passw)
-        if user and user.user:
-            st.session_state.user_email=user.user.email
-            st.success(f'Welcome back {email}')
-            st.rerun()
-
-if 'user_email' not in st.session_state:
-    st.session_state.user_email=None
-
-if st.session_state.user_email:
-    main_app(st.session_state.user_email)
-else:
-    auth_screen()
